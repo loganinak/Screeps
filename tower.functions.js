411efdac6ecc23346f053b26c0d9eb6eca03183e
Logan Bonney <wakeup2early@gmail.com>
2020-12-12 00:01:48 -0700

    Made tower not report ERR_NOT_ENOUGH_ENERGY

--- tower.functions.js
+++ tower.functions.js
@@ -5,7 +5,7 @@ const towerFunctions = {
     const repairTarget = tower.pos.findClosestByRange(selectors.repairTargets(tower.room, 1));
     const repairResult = tower.repair(repairTarget);
 
-    if (repairResult != OK && repairTarget) {
+    if (repairResult != OK && repairTarget && repairResult != ERR_NOT_ENOUGH_ENERGY) {
       console.log("Tower idling repair error: " + repairResult);
     }
   }
