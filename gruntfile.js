b621d44615a1d68e5152dbc70adcf8ab65b86872
Logan Bonney <wakeup2early@gmail.com>
2020-12-19 13:31:06 -0700

    fixed grunt file

--- gruntfile.js
+++ gruntfile.js
@@ -1,28 +1,23 @@
 module.exports = function(grunt) {
 
-    var config = require('./.screeps.json');
-    var branch = grunt.option('branch') || config.branch;
+  var config = require('./.screeps.json');
+  var branch = grunt.option('branch') || config.branch;
 
-    grunt.loadNpmTasks('grunt-screeps');
+  grunt.loadNpmTasks('grunt-screeps');
 
-    grunt.initConfig({
-        screeps: {
-            options: {
-                email: config.email,
-                token: config.token,
-                branch: branch,
-                // server: 'season'
-            },
-            dist: {
-                files: [
-                    {
-                        expand: true,
-                        cwd: 'dist/',
-                        src: ['**/*.{js,wasm}'],
-                        flatten: true
-                    }
-                ]
-            }
-        }
-    });
+  grunt.initConfig({
+    screeps: {
+      options: {
+        email: config.email,
+        token: config.token,
+        branch: branch,
+        // server: 'season'
+      },
+      dist: {
+        files: [{
+          src: ['dist/*.js'],
+        }]
+      }
+    }
+  });
 }
